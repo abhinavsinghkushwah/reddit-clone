5e8d1522e03aea6c5f75d6e838f30c4842da2f64
Abhinav <abhinavkushwah20@gmail.com>
2023-02-15 00:44:27 +0530

    added async to reduce api response time

--- src/main/java/com/abhinav/reddit/service/MailService.java
+++ src/main/java/com/abhinav/reddit/service/MailService.java
@@ -5,6 +5,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.abhinav.reddit.exceptions.SpringRedditException;
@@ -21,6 +22,7 @@ public class MailService {
 	private final JavaMailSender mailSender;
 	private final MailContentBuilder mailContentbuilder;
 	
+	@Async
 	public void sendMail(NotificationEmail notificationEmail) {
 		MimeMessagePreparator messagePerparator= mimeMessage ->{
 			MimeMessageHelper messageHelper= new MimeMessageHelper(mimeMessage);
